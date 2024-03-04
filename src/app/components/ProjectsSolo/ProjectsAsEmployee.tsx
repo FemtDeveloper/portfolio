8ed53a0e15b7d3df5fa8828ce4b902a3c7f87309
FELIX EDUARDO MIRANDA TERAN <feedmite@hotmail.com>
2024-03-04 03:09:43 -0500

    more projeccts

--- src/app/components/ProjectsSolo/ProjectsAsEmployee.tsx
+++ src/app/components/ProjectsSolo/ProjectsAsEmployee.tsx
@@ -5,9 +5,9 @@ const ProjectsAsEmployee = () => {
   return (
     <section className="outer-container w-full h-full relative">
       <div className="slider-overflow-container w-full overflow-x-auto">
-        <div className="slider flex w-[150vw] justify-start px-4 lg:px-0 gap-20">
+        <div className="slider flex w-[200vw] md:w-[150vw] justify-start px-4 lg:px-0 gap-10">
           {PROJECTS_AS_EMPLOYEE.map((project) => (
-            <div className="inner flex justify-center " key={project.id}>
+            <div className="inner flex justify-center w-full" key={project.id}>
               <Project mainProps={project} isInSlider maxWidth="90%" />
             </div>
           ))}
