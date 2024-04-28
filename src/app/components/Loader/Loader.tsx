660478a21e849afbfb4a2f9f66f06adc7368d93a
FELIX EDUARDO MIRANDA TERAN <feedmite@hotmail.com>
2024-04-28 13:32:36 -0500

    loader updated

--- src/app/components/Loader/Loader.tsx
+++ src/app/components/Loader/Loader.tsx
@@ -1,7 +1,5 @@
 "use client";
-import Spline from "@splinetool/react-spline";
-import clsx from "clsx";
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Loader = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -12,19 +10,8 @@ const Loader = () => {
   }, [isLoading]);
   return (
     <div className="h-full w-full flex items-center justify-center bg-black">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Spline scene="https://prod.spline.design/WFzUKorGO5F46ifn/scene.splinecode" />
-      </Suspense>
-      {/* <h2
-        className={clsx(
-          "font-bold text-white  tracking-wider transition-all opacity-0 transform duration-2000",
-          isLoading
-            ? "scale-50 text-[20vh]"
-            : "scale-150 text-[5050vh] opacity-100"
-        )}
-      >
-        Motion
-      </h2> */}
+      {/* <Spline scene="https://prod.spline.design/WFzUKorGO5F46ifn/scene.splinecode" /> */}
+      <video src="/assets/light.mp4" autoPlay muted />
     </div>
   );
 };
