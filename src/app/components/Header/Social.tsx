b8a24bbaea0c1176be4fbb716462e8435832e91a
FELIX EDUARDO MIRANDA TERAN <feedmite@hotmail.com>
2024-12-26 11:33:28 -0500

    ajuste social networks

--- src/app/components/Header/Social.tsx
+++ src/app/components/Header/Social.tsx
@@ -1,22 +1,25 @@
 import { useThemeStore } from "@/store/useThemeStore";
+import Link from "next/link";
 import { GithubIcon, LinkedInIcon } from "../../../Icons";
 
 const Social = () => {
   const theme = useThemeStore((state) => state.theme);
   return (
     <div className="flex gap-4 justify-center md:justify-start">
-      <a
+      <Link
         href="https://github.com/FemtDeveloper"
         aria-label="Link that redirects to githup profile"
+        target="_blank"
       >
         <GithubIcon color={theme === "dark" ? "#fff" : "#000"} />
-      </a>
-      <a
+      </Link>
+      <Link
         href="https://www.linkedin.com/in/fmirandat/"
         aria-label="Link that redirects to linkedin profile"
+        target="_blank"
       >
         <LinkedInIcon color={theme === "dark" ? "#fff" : "#000"} />
-      </a>
+      </Link>
     </div>
   );
 };
